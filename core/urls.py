63c13265a3025fd1005ea79febae9cebfdc94360
proj-int01 <adilson.monte.alto@gmail.com>
2022-07-16 09:20:25 -0300

    primeiro teste add pagina produto

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, produto
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('produto/', produto, name='produto'),
     
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from .forms import ProdutoModelForm # retirado ContatoForm,
+from .models import Produto
+from django.shortcuts import redirect #usado para redirecionar usuário anonimo.
+
 
 def index(request):
     return render(request, 'index.html')
+
+def produto(request):
+    if str(request.user) != 'AnonymousUser': # verifica se o usuário está logado
+        if str(request.method) == 'POST':
+            form = ProdutoModelForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Produto salvo com sucesso.')
+                form = ProdutoModelForm() # limpa as entradas do form na página
+            else:
+                messages.error(request, 'Erro ao salvar o produto.')
+
+        else:
+            form = ProdutoModelForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'produto.html', context )
+    else:
+        return redirect('index')
