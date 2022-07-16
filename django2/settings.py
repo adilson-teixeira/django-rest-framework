fef6312b9c53b56af2d0bd48dc487f327e3c7f1a
proj-int01 <adilson.monte.alto@gmail.com>
2022-07-16 08:18:05 -0300

    modify settings

--- django2/settings.py
+++ django2/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'django2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
