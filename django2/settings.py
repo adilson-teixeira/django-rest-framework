04806fdb4c671f5de77a58a48bab9207b3f42093
proj-int01 <adilson.monte.alto@gmail.com>
2022-07-16 08:07:41 -0300

    debug true

--- django2/settings.py
+++ django2/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!cpupg4^nt6s=5_odx1wner6#j@tv65)c7sg6n3mxki%g(4o)v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
